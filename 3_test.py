5b14f4099b8a3878f8ab19f935ca1e78ce7e6dfb
Ratthasart Rojjanai <feevfeev2@gmail.com>
2025-07-21 13:21:45 +0700

    up

--- 3_test.py
+++ 3_test.py
@@ -24,14 +24,7 @@ msg = 'Ratto'
 msg = msg.encode('utf-8') # แปลง msg ให้เป็นรหัสของ utf-8
 key = b'Rojjanai'
 hmac_val2 = hmac.new(key, msg, hashlib.sha256).hexdigest() # แปลงคีย์ที่ได้รับคือ key และ msg เป็นรหัสของ sha256 โดยใช้ hmac, hashlib
-<<<<<<< HEAD
 test = hmac.compare_digest(hmac_val1, hmac_val2) # เปรียบเทียบ ผู้รับ และ ผู้ส่งว่าตรงกันไหม
 print("hmac_val2 : ", hmac_val2) # 51cee9642d9578e829b82f6de8c148720345cdef0429852d66ca1a48c80a7a07
 
 print("เปรียบเทียบ hmac_val1 กับ hmac_val2 :", test) # False
-=======
-hmac.compare_digest(hmac_val1, hmac_val2) # เปรียบเทียบ ผู้รับ และ ผู้ส่งว่าตรงกันไหม
-print("hmac_val2 : ", hmac_val2) # 51cee9642d9578e829b82f6de8c148720345cdef0429852d66ca1a48c80a7a07
-
-# AIzaSyBgjNW0WA93qphgZW-joXVR6VC3IiYFjfo
->>>>>>> 4572963f1d1a19676e11c9b3471ca75b2ab5f9f5
